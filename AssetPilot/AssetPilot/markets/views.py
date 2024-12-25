3a2d6b35f377ef3c50a3cb34e76e2c27451effcb
Daniel Stefanov <97540472+ludsmotika@users.noreply.github.com>
2024-12-25 14:53:16 +0200

    fix suggestions when searching for a ticker. Change the stock information provider to finnhub

--- AssetPilot/AssetPilot/markets/views.py
+++ AssetPilot/AssetPilot/markets/views.py
@@ -1,52 +1,58 @@
+import os
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from .models import Trade
-import yfinance as yf
+import finnhub
+from django.conf import settings
+
+FINNHUB_API_KEY = os.environ.get("FINNHUB_API_KEY")
+finnhub_client = finnhub.Client(FINNHUB_API_KEY)
 
 
 @login_required
 def markets_home_view(request):
-
     ticker = request.GET.get("q", "AAPL")
 
     if request.method == "POST":
-        trade_type = request.POST.get("order_type").upper()
-        amount = request.POST.get("amount")
+        try:
+            trade_type = request.POST.get("order_type").upper()
+            amount = request.POST.get("amount")
 
-        stock = yf.Ticker(ticker)
-        current_price = stock.fast_info["lastPrice"]
+            quote = finnhub_client.quote(ticker)
+            current_price = quote["c"]
 
-        Trade.objects.create(
-            user=request.user,
-            ticker=ticker,
-            trade_type=trade_type,
-            amount=amount,
-            price=current_price,
-        )
+            Trade.objects.create(
+                user=request.user,
+                ticker=ticker,
+                trade_type=trade_type,
+                amount=amount,
+                price=current_price,
+            )
+
+        except Exception as e:
+            return JsonResponse({"status": "error", "message": str(e)}, status=400)
     elif request.method == "GET":
         context = {"search_query": ticker}
         return render(request, "markets/markets.html", context)
 
 
 def search_stocks(request):
-    query = request.GET.get("q", "").upper()
+    query = request.GET.get("q", "").strip()
     if len(query) < 2:
         return JsonResponse([], safe=False)
 
-    search_results = yf.Tickers(query)
+    search_results = finnhub_client.symbol_lookup(query)
+
     results = []
 
-    for result in search_results:
-
-        try:
-            ticker = yf.Ticker(result)
-            info = ticker.info
-            results.append({"symbol": result, "name": info.get("longName", "")})
-        except:
-            continue
-
-        if len(results) >= 10:
-            break
+    for item in search_results.get("result", [])[:10]:
+        if item.get("type") == "Common Stock":
+            results.append(
+                {
+                    "symbol": item.get("symbol", ""),
+                    "name": item.get("description", ""),
+                }
+            )
 
     return JsonResponse(results, safe=False)
