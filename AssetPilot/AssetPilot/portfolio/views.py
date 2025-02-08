444a35ba00a61c20814a8989d6d5f0f2c2251c65
Daniel Stefanov <97540472+danielkstefanov@users.noreply.github.com>
2025-02-08 23:54:00 +0200

    Add portfolio suggestions

--- AssetPilot/AssetPilot/portfolio/views.py
+++ AssetPilot/AssetPilot/portfolio/views.py
@@ -3,7 +3,35 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from markets.models import Trade
-from utils.trading import get_ticker_price
+from utils.trading import get_ticker_price, get_pe_ratio, get_rsi
+
+
+SUGGESTIONS_EXPLANATIONS = [
+    {
+        "text": "Based on financial principles, you should diversify your portfolio. You should think about the allocation that you have in: ",
+        "indicator": "allocation",
+        "to_use_less_comparator": False,
+        "value": 20,
+    },
+    {
+        "text": "It is normal for a stock to have a P/E ratio of 15-20. If a stock has a P/E ratio of 20 or more, it is overvalued and you should consider selling it. In your portfolio, the following stocks doesn't follow this principle: ",
+        "indicator": "pe_ratio",
+        "to_use_less_comparator": False,
+        "value": 20,
+    },
+    {
+        "text": "If a stock has an RSI of 70 or more, it is overvalued and you should consider selling it. In your portfolio, the following stocks doesn't follow this principle: ",
+        "indicator": "rsi",
+        "to_use_less_comparator": False,
+        "value": 70,
+    },
+    {
+        "text": "If a stock has an RSI of 30 or less, it is undervalued and you should consider buying it. In your portfolio, the following stocks are considered undervalued: ",
+        "indicator": "rsi",
+        "to_use_less_comparator": True,
+        "value": 30,
+    },
+]
 
 
 @login_required
@@ -16,6 +44,9 @@ def portfolio(request):
         enter_price = float(trade.enter_price)
 
         profit_loss_percentage = ((current_price - enter_price) / enter_price) * 100
+
+        if trade.trade_type == "SELL":
+            profit_loss_percentage *= -1
 
         open_trades_data.append(
             {
@@ -61,10 +92,7 @@ def portfolio_data(request):
         for ticker, percentage in allocation.items()
     ]
 
-    suggestions = []
-    for symbol, percentage in allocation.items():
-        if percentage > 20:
-            suggestions.append(f"You have too much allocation in {symbol}")
+    suggestions = create_suggestions(allocation)
 
     return JsonResponse(
         {
@@ -77,6 +105,48 @@ def portfolio_data(request):
             "portfolio_return_value": total_value - protfolio_original_size,
         }
     )
+
+
+def create_suggestions(allocation):
+
+    suggestions = {
+        "our_suggestions": [],
+        "ai_suggestions": [],
+    }
+
+    for suggestion in SUGGESTIONS_EXPLANATIONS:
+        current_suggestion = suggestion["text"]
+        to_add = False
+
+        for ticker, percentage in allocation.items():
+            pe_ratio = get_pe_ratio(ticker)
+            rsi_value = get_rsi(ticker)
+
+            value_to_compare = {
+                "allocation": percentage,
+                "pe_ratio": pe_ratio,
+                "rsi": rsi_value,
+            }[suggestion["indicator"]]
+
+            if follows_suggestion(
+                value_to_compare,
+                suggestion["to_use_less_comparator"],
+                suggestion["value"],
+            ):
+                current_suggestion += f"{ticker}, "
+                to_add = True
+
+        if to_add:
+            suggestions["our_suggestions"].append(current_suggestion[:-2])
+
+    return suggestions
+
+
+def follows_suggestion(value_to_compare, to_use_less_comparator, value):
+    if to_use_less_comparator:
+        return value_to_compare < value
+    else:
+        return value_to_compare > value
 
 
 @login_required
@@ -98,6 +168,9 @@ def trade_detail(request, trade_id):
     profit_loss = (
         (current_price - float(trade.enter_price)) / float(trade.enter_price)
     ) * 100
+
+    if trade.trade_type == "SELL":
+        profit_loss *= -1
 
     context = {
         "trade": trade,
--- AssetPilot/AssetPilot/utils/trading.py
+++ AssetPilot/AssetPilot/utils/trading.py
@@ -1,4 +1,5 @@
 from tradingview_ta import TA_Handler, Interval
+import yfinance as yf
 
 configurations = [
     {"exchange": "NASDAQ", "screener": "america"},
@@ -22,3 +23,29 @@ def get_ticker_price(ticker):
             continue
 
     raise ValueError(f"Could not find price for ticker: {ticker}")
+
+
+def get_pe_ratio(ticker):
+    try:
+        stock = yf.Ticker(ticker)
+        info = stock.info
+        return info.get("trailingPE", None)
+    except Exception as e:
+        return None
+
+
+def get_rsi(ticker):
+    for configuration in configurations:
+        try:
+            handler = TA_Handler(
+                symbol=ticker,
+                exchange=configuration["exchange"],
+                screener=configuration["screener"],
+                interval=Interval.INTERVAL_1_DAY,
+            )
+            analysis = handler.get_analysis()
+            return float(analysis.indicators["RSI"])
+        except:
+            continue
+
+    return None
